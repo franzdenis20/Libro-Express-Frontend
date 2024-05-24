b9f5a29ec1267aee840828a3445f07333a5107cf
franzdenis20 <franzchoquevillaca@gmail.com>
2024-05-23 20:12:33 -0400

    segundo commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const instance =  axios.create({
     withCredentials: true,
-    baseURL: "http://localhost:4000/api"
-    // baseURL: 'https://viajes-node.onrender.com/api'
+    // baseURL: "http://localhost:4000/api"
+    baseURL: 'https://libro-express-backend.onrender.com'
     
 })
 export default instance;
