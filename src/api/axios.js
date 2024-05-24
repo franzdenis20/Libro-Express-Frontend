41999435016e002825068e882d4919a181afd4dc
franzdenis20 <franzchoquevillaca@gmail.com>
2024-05-24 00:18:42 -0400

    cuarto commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance =  axios.create({
     withCredentials: true,
     // baseURL: "http://localhost:4000/api"
-    baseURL: 'https://libro-express-backend.onrender.com'
+    baseURL: 'https://libro-express-backend.onrender.com/api'
     
 })
 export default instance;
